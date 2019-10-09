fee5013a0cc9f0069e0523a30300801e55bf6933
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-10-09 20:31:47 +0700

    WIP

--- web/src/app/shared/post/post-author.component.ts
+++ web/src/app/shared/post/post-author.component.ts
@@ -5,9 +5,24 @@ import { PostComponent } from './post.component';
 @Component({
   selector: 'app-post-author',
   template: `
-    <ng-content></ng-content>
+    <img src="assets/images/303589.png" class="avatar" />
+    <div class="display-name">Natcha Luangaroonchai</div>
   `,
-  styles: [],
+  styles: [
+    `
+      .avatar {
+          border-radius: 50%;
+          height: 6.4rem;
+          width: 6.4rem;
+      }
+    `,
+    `
+      .display-name {
+          color: #333;
+          font: normal 400 1.6rem Lato, sans-serif;
+      }
+    `,
+  ],
 })
 export class PostAuthorComponent extends PostComponent implements OnInit {
 
--- web/src/app/shared/post/post.module.ts
+++ web/src/app/shared/post/post.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { PostAuthorComponent } from '../post.old/post-author.component';
+import { PostAuthorComponent } from './post-author.component';
 import { PostCategoriesComponent } from './post-categories.component';
 import { PostExcerptComponent } from './post-excerpt.component';
+import { PostMetadataComponent } from './post-metadata.component';
 import { PostTitleComponent } from './post-title.component';
 
 @NgModule({
@@ -14,12 +15,14 @@ import { PostTitleComponent } from './post-title.component';
     PostAuthorComponent,
     PostCategoriesComponent,
     PostExcerptComponent,
+    PostMetadataComponent,
     PostTitleComponent,
   ],
   exports: [
     PostAuthorComponent,
     PostCategoriesComponent,
     PostExcerptComponent,
+    PostMetadataComponent,
     PostTitleComponent,
   ],
 })
