96037464084edaca88bb4919998e24479f1e7c55
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-10-14 09:37:23 +0700

    Style outline button

--- web/src/app/shared/button/outline-button.component.ts
+++ web/src/app/shared/button/outline-button.component.ts
@@ -2,9 +2,33 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-outline-button',
-  template: ``,
-  styles: [],
+  template: `
+    <button>
+      <ng-content></ng-content>
+    </button>
+  `,
+  styles: [
+    `
+      :host {
+          align-items: center;
+          cursor: pointer;
+          display: inline-flex;
+          height: 100%;
+          justify-content: center;
+      }
+    `,
+    `
+      button {
+          background: none;
+          border: .1rem solid #b3b3b3;
+          border-radius: 100vh;
+          color: #4d4d4d;
+          font: normal 300 1.6rem Lato, sans-serif;
+          min-width: 12.8rem;
+          min-height: 4rem;
+          padding: 0;
+      }
+    `,
+  ],
 })
-export class OutlineButtonComponent {
-
-}
+export class OutlineButtonComponent { }
