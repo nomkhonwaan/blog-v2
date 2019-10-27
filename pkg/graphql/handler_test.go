f0250aea359f026d4500d7d1fcb3e169dc94cb33
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-10-27 10:45:22 +0700

    Add more test cases on finding the post by its ID

--- pkg/graphql/handler_test.go
+++ pkg/graphql/handler_test.go
@@ -4,11 +4,15 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/golang/mock/gomock"
+	"github.com/nomkhonwaan/myblog/pkg/auth"
 	"github.com/nomkhonwaan/myblog/pkg/blog"
 	mock_blog "github.com/nomkhonwaan/myblog/pkg/blog/mock"
 	. "github.com/nomkhonwaan/myblog/pkg/graphql"
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -28,12 +32,20 @@ func TestHandler(t *testing.T) {
 		catRepo  = mock_blog.NewMockCategoryRepository(ctrl)
 		postRepo = mock_blog.NewMockPostRepository(ctrl)
 		tagRepo  = mock_blog.NewMockTagRepository(ctrl)
-
-		newGraphQLRequest = func(q query) *http.Request {
-			v, _ := json.Marshal(q)
-			return httptest.NewRequest(http.MethodPost, "/graphql", bytes.NewReader(v))
-		}
 	)
+
+	newGraphQLRequest := func(q query) *http.Request {
+		v, _ := json.Marshal(q)
+		return httptest.NewRequest(http.MethodPost, "/graphql", bytes.NewReader(v))
+	}
+
+	withAuthorizedID := func(r *http.Request) *http.Request {
+		return r.WithContext(context.WithValue(context.Background(), auth.UserProperty, &jwt.Token{
+			Claims: jwt.MapClaims{
+				"sub": "authorizedID",
+			},
+		}))
+	}
 
 	service.EXPECT().Category().Return(catRepo).AnyTimes()
 	service.EXPECT().Post().Return(postRepo).AnyTimes()
@@ -76,5 +88,106 @@ func TestHandler(t *testing.T) {
 
 		// Then
 		assert.Equal(t, "200 OK", w.Result().Status)
+	})
+
+	t.Run("Find post by its ID", func(t *testing.T) {
+		t.Run("With existing published post", func(t *testing.T) {
+			// Given
+			expected := primitive.NewObjectID()
+			slug := "test-published-" + expected.Hex()
+			q := query{Query: `{ post(slug: "` + slug + `") { slug } }`}
+
+			w := httptest.NewRecorder()
+
+			postRepo.EXPECT().FindByID(gomock.Any(), gomock.Any()).DoAndReturn(func(_ context.Context, id interface{}) (blog.Post, error) {
+				assert.IsType(t, primitive.ObjectID{}, id)
+				assert.Equal(t, expected.Hex(), id.(primitive.ObjectID).Hex())
+
+				return blog.Post{Status: blog.Published}, nil
+			})
+
+			// When
+			h.ServeHTTP(w, newGraphQLRequest(q))
+
+			// Then
+			assert.Equal(t, "200 OK", w.Result().Status)
+		})
+
+		t.Run("With existing draft post", func(t *testing.T) {
+			// Given
+			expected := primitive.NewObjectID()
+			slug := "test-draft-" + expected.Hex()
+			q := query{Query: `{ post(slug: "` + slug + `") { slug } }`}
+
+			w := httptest.NewRecorder()
+
+			postRepo.EXPECT().FindByID(gomock.Any(), gomock.Any()).Return(blog.Post{Status: blog.Draft, AuthorID: "authorizedID"}, nil)
+
+			// When
+			h.ServeHTTP(w, withAuthorizedID(newGraphQLRequest(q)))
+
+			// Then
+			assert.Equal(t, "200 OK", w.Result().Status)
+		})
+
+		t.Run("When an error has occurred while querying post", func(t *testing.T) {
+			// Given
+			expected := primitive.NewObjectID()
+			slug := "test-published-" + expected.Hex()
+			q := query{Query: `{ post(slug: "` + slug + `") { slug } }`}
+
+			w := httptest.NewRecorder()
+
+			postRepo.EXPECT().FindByID(gomock.Any(), gomock.Any()).Return(blog.Post{}, errors.New("test error on finding post by ID"))
+
+			// When
+			h.ServeHTTP(w, newGraphQLRequest(q))
+
+			// Then
+			var result map[string]interface{}
+			_ = json.NewDecoder(w.Body).Decode(&result)
+
+			assert.Equal(t, "post: test error on finding post by ID", result["errors"].([]interface{})[0].(string))
+		})
+
+		t.Run("With existing draft post but unable to retrieve authorized ID", func(t *testing.T) {
+			// Given
+			expected := primitive.NewObjectID()
+			slug := "test-draft-" + expected.Hex()
+			q := query{Query: `{ post(slug: "` + slug + `") { slug } }`}
+
+			w := httptest.NewRecorder()
+
+			postRepo.EXPECT().FindByID(gomock.Any(), gomock.Any()).Return(blog.Post{Status: blog.Draft}, nil)
+
+			// When
+			h.ServeHTTP(w, newGraphQLRequest(q))
+
+			// Then
+			var result map[string]interface{}
+			_ = json.NewDecoder(w.Body).Decode(&result)
+
+			assert.Equal(t, "post: Unauthorized", result["errors"].([]interface{})[0].(string))
+		})
+
+		t.Run("With existing draft post but not post's author", func(t *testing.T) {
+			// Given
+			expected := primitive.NewObjectID()
+			slug := "test-draft-" + expected.Hex()
+			q := query{Query: `{ post(slug: "` + slug + `") { slug } }`}
+
+			w := httptest.NewRecorder()
+
+			postRepo.EXPECT().FindByID(gomock.Any(), gomock.Any()).Return(blog.Post{Status: blog.Draft, AuthorID: "otherAuthorizedID"}, nil)
+
+			// When
+			h.ServeHTTP(w, withAuthorizedID(newGraphQLRequest(q)))
+
+			// Then
+			var result map[string]interface{}
+			_ = json.NewDecoder(w.Body).Decode(&result)
+
+			assert.Equal(t, "post: Forbidden", result["errors"].([]interface{})[0].(string))
+		})
 	})
 }
