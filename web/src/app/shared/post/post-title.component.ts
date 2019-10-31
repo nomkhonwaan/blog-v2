1142efbbc569f58d147a40726315dc684e422acb
Natcha Luangaroonchai <natcha.lu@kbtg.tech>
2019-10-31 09:26:21 +0700

    Diable anchor link on a single page title

--- web/src/app/shared/post/post-title.component.ts
+++ web/src/app/shared/post/post-title.component.ts
@@ -1,49 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { PostComponent } from './post.component';
 
 @Component({
   selector: 'app-post-title',
   template: `
-    <a [routerLink]="link">
+    <a [routerLink]="isDisabledLink() ? null : href">
       <ng-content></ng-content>
     </a>
   `,
   styles: [
     `
       :host {
-          color: #333;
-          font-family: Athiti, sans-serif;
-          font-weight: 500;
+        color: #333;
+        font-family: Athiti, sans-serif;
+        font-weight: 500;
       }
     `,
     `
       ::ng-deep h1 {
-          font-size: 4.8rem;
-          font-weight: inherit;
+        font-size: 4.8rem;
+        font-weight: inherit;
       }
     `,
     `
       ::ng-deep h2 {
-          font-size: 3.6rem;
-          font-weight: inherit;
+        font-size: 3.6rem;
+        font-weight: inherit;
       }
     `,
   ],
 })
 export class PostTitleComponent extends PostComponent implements OnInit {
 
-  link: string[];
+  /**
+   * Used to indiciate whether link or no-link
+   */
+  @Input()
+  link: boolean = true;
+
+  href: string[];
 
   ngOnInit(): void {
     const publishedAt: Date = new Date(this.post.publishedAt);
 
-    this.link = [
-      publishedAt.getFullYear().toString(),
-      publishedAt.getMonth().toString(),
-      publishedAt.getDate().toString(),
-      this.post.slug,
-    ];
+    if (this.link) {
+      this.href = [publishedAt.getFullYear().toString(), publishedAt.getMonth().toString(), publishedAt.getDate().toString(), this.post.slug];
+    }
+  }
+
+  isDisabledLink(): boolean {
+    return !!!this.link;
   }
 
 }
