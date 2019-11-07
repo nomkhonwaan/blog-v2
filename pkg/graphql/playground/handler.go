a668790474905cb93caa528e848395f1e91cf157
Natcha Luangaroonchai <natcha.lu@kbtg.tech>
2019-11-07 08:51:29 +0700

    Change Graphiql handler function to handler
    Signed-off-by: Natcha Luangaroonchai <natcha.lu@kbtg.tech>

--- pkg/graphql/playground/handler.go
+++ pkg/graphql/playground/handler.go
@@ -2,11 +2,11 @@ package playground
 
 import "net/http"
 
-// HandlerFunc provides GraphQL Playground handler
-func HandlerFunc(gzipAsset []byte) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, _ *http.Request) {
+// Handler provides Graphiql for playing on GraphQL server
+func Handler(data []byte) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Encoding", "gzip")
 		w.Header().Set("Content-Type", "text/html")
-		_, _ = w.Write(gzipAsset)
-	}
+		_, _ = w.Write(data)
+	})
 }
