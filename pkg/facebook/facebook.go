57b94b6e97f62fdb67eca8838698e071c2a7e138
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-11-09 14:34:04 +0700

    Use post's featured image first, then fallback to the default image

--- pkg/facebook/facebook.go
+++ pkg/facebook/facebook.go
@@ -2,11 +2,17 @@ package facebook
 
 import (
 	"github.com/nomkhonwaan/myblog/pkg/blog"
+	"github.com/nomkhonwaan/myblog/pkg/storage"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
 	"regexp"
 	"strings"
 	"text/template"
+	"time"
+)
+
+var (
+	bangkokTimeZone, _ = time.LoadLocation("Asia/Bangkok")
 )
 
 // IsFacebookCrawlerRequest does checking the request user-agent strings.
@@ -30,14 +36,22 @@ func IsSingle(url string) (string, bool) {
 type Service interface {
 	// A Post repository
 	Post() blog.PostRepository
+
+	// A File repository
+	File() storage.FileRepository
 }
 
 type service struct {
 	postRepo blog.PostRepository
+	fileRepo storage.FileRepository
 }
 
 func (s service) Post() blog.PostRepository {
 	return s.postRepo
+}
+
+func (s service) File() storage.FileRepository {
+	return s.fileRepo
 }
 
 // CrawlerMiddleware is a Facebook specific middleware
@@ -48,14 +62,17 @@ type CrawlerMiddleware struct {
 }
 
 // NewCrawlerMiddleware returns a Facebook's crawler specific middleware instance
-func NewCrawlerMiddleware(openGraphTemplate string, postRepo blog.PostRepository) (CrawlerMiddleware, error) {
+func NewCrawlerMiddleware(openGraphTemplate string, postRepo blog.PostRepository, fileRepo storage.FileRepository) (CrawlerMiddleware, error) {
 	t, err := template.New("facebook-opengraph-template").Parse(openGraphTemplate)
 	if err != nil {
 		return CrawlerMiddleware{}, err
 	}
 
 	return CrawlerMiddleware{
-		service:  service{postRepo: postRepo},
+		service: service{
+			postRepo: postRepo,
+			fileRepo: fileRepo,
+		},
 		template: t,
 	}, nil
 }
@@ -83,10 +100,17 @@ func (mw CrawlerMiddleware) serveSingle(w http.ResponseWriter, r *http.Request, 
 		return
 	}
 
-	// Do not allow unpublished post to be shared
 	if p.Status != blog.Published {
 		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 		return
+	}
+
+	featuredImage := "https://beta.nomkhonwaan.com/assets/images/303589.webp"
+	if !p.FeaturedImage.ID.IsZero() {
+		file, _ := mw.service.File().FindByID(r.Context(), p.FeaturedImage.ID)
+		if file.Slug != "" {
+			featuredImage = "https://beta.nomkhonwaan.com/api/v2/storage/" + file.Slug
+		}
 	}
 
 	data := struct {
@@ -96,11 +120,11 @@ func (mw CrawlerMiddleware) serveSingle(w http.ResponseWriter, r *http.Request, 
 		Description   string
 		FeaturedImage string
 	}{
-		URL:           "https://beta.nomkhonwaan.com/" + p.PublishedAt.Format("2006/1/2") + "/" + p.Slug,
+		URL:           "https://beta.nomkhonwaan.com/" + p.PublishedAt.In(bangkokTimeZone).Format("2006/1/2") + "/" + p.Slug,
 		Type:          "article",
 		Title:         p.Title,
 		Description:   strings.Split(p.Markdown, "\n")[0],
-		FeaturedImage: "https://beta.nomkhonwaan.com/assets/images/303589.webp",
+		FeaturedImage: featuredImage,
 	}
 
 	if err = mw.template.Execute(w, data); err != nil {
