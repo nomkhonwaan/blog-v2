0e6326602ec2235d7eb48617c7ecf50ea7b6eb55
Natcha Luangaroonchai <natcha.lu@kbtg.tech>
2019-11-14 08:04:29 +0700

    Add new interfaces for engagement and file object

--- pkg/blog/engagement.go
+++ pkg/blog/engagement.go
@@ -2,5 +2,6 @@ package blog
 
 // Engagement represents social network engagement of the object
 type Engagement struct {
+	// Total object shared counter
 	ShareCount int `bson:"-" json:"shareCount" graphql:"shareCount"`
 }
