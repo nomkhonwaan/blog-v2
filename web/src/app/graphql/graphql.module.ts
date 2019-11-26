447e1b294a77cb7cdd50253ffa99a1b524775b1a
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-11-26 21:18:46 +0700

    [WIP] Auto-update HTTP headers on Apollo client

--- web/src/app/graphql/graphql.module.ts
+++ web/src/app/graphql/graphql.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
+import { Store, select } from '@ngrx/store';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { GraphQLRequest, ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
+
 import { environment } from 'src/environments/environment';
 
 const uri = environment.graphql.endpoint;
 
-export function createApollo(httpLink: HttpLink) {
+export function createApollo(store: Store<AppState>, httpLink: HttpLink) {
+  // let bearer: string;
+
+  // store.pipe(select('app', 'auth', 'accessToken')).subscribe((accessToken: string): void => {
+  //   if (accessToken !== '') {
+  //     bearer = `Bearer ${accessToken}`;
+  //   }
+  // });
+
+  // const auth: ApolloLink = setContext((operation: GraphQLRequest, prevContext: any): Promise<any> | any => {
+  //   if (bearer !== '') {
+  //     return {
+  //       headers: {
+  //         Authorization: bearer,
+  //       },
+  //     };
+  //   } else {
+  //     return {
+  //       headers: {},
+  //     };
+  //   }
+  // });
+
   return {
-    link: httpLink.create({ uri }),
+    link: ApolloLink.from([httpLink.create({ uri, withCredentials: true })]),
     cache: new InMemoryCache(),
   };
 }
@@ -19,7 +45,7 @@ export function createApollo(httpLink: HttpLink) {
     {
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
-      deps: [HttpLink],
+      deps: [Store, HttpLink],
     },
   ],
 })
