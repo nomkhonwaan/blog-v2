391f843afab4ef8ff2e6ed7e6e00cfc99c19248c
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-12-05 08:50:04 +0700

    Rename action `setAuthorization` to `setAuthentication`

--- web/src/app/app.reducer.ts
+++ web/src/app/app.reducer.ts
@@ -1,7 +1,12 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import update from 'immutability-helper';
 
-import { toggleSidebar, setAuthorization, isFetching, isNotFetching } from './app.actions';
+import {
+  isFetching,
+  isNotFetching,
+  setAuthentication,
+  toggleSidebar,
+} from './app.actions';
 
 const initialState: AppState = {
   isFetching: false,
@@ -15,7 +20,7 @@ const appReducer = createReducer(
   initialState,
   on(isFetching, (state) => update<AppState>(state, { isFetching: { $set: true } })),
   on(isNotFetching, (state) => update<AppState>(state, { isFetching: { $set: false } })),
-  on(setAuthorization, (state, { accessToken }) => update<AppState>(state, { auth: { accessToken: { $set: accessToken } } })),
+  on(setAuthentication, (state, { accessToken }) => update<AppState>(state, { auth: { accessToken: { $set: accessToken } } })),
   on(toggleSidebar, (state) => update<AppState>(state, { sidebar: { $toggle: ['collapsed'] } })),
 );
 
