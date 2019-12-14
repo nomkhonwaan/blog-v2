22558628455b4b5dd5a5eb2bc17cb6cdd25ae3e4
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-12-15 06:05:48 +0700

    Typo

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth/auth.guard';
+import { AuthGuard } from './auth';
 import { LoginComponent } from './login.component';
 import { LogoutComponent } from './logout.component';
 
@@ -18,13 +18,13 @@ const routes: Routes = [
   {
     path: 'admin',
     canActivate: [AuthGuard],
-    loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule),
+    loadChildren: () => import('./admin').then((m) => m.AdminModule),
   },
   {
     path: ':year/:month/:date/:slug/edit',
     pathMatch: 'full',
     canActivate: [AuthGuard],
-    loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule),
+    loadChildren: () => import('./admin').then((m) => m.AdminModule),
   },
   {
     path: '',
