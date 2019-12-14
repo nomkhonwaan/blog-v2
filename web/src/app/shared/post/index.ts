67eee564e97e695cb017c71960e42d4f89b75bc7
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-12-14 19:59:57 +0700

    Refactor post metadata

--- web/src/app/shared/post/index.ts
+++ web/src/app/shared/post/index.ts
@@ -1,4 +1,7 @@
 export * from './author';
+export * from './categories';
 export * from './metadata';
-export * from './post.module';
 export * from './post.actions';
+export * from './post.module';
+export * from './share-to';
+
