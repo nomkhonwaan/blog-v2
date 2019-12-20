bcf54df1c1e6bbdb0be5b37ea1ae232719b6453e
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-12-20 08:56:55 +0700

    Auto-save on keypress event (#47)

--- web/src/app/admin/editor/markdown/markdown.component.ts
+++ web/src/app/admin/editor/markdown/markdown.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AbstractPostEditorComponent } from '../abstract-post-editor.component';
+import { BehaviorSubject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-post-markdown-editor',
   templateUrl: './markdown.component.html',
   styleUrls: ['./markdown.component.scss'],
 })
-export class PostMarkdownEditorComponent extends AbstractPostEditorComponent {
+export class PostMarkdownEditorComponent extends AbstractPostEditorComponent implements OnInit {
+
+  /**
+   * Use for debouncing keypress event on markdown value
+   */
+  markdown$: BehaviorSubject<string> = new BehaviorSubject('');
+
+  ngOnInit(): void {
+    this.markdown$.next(this.post.markdown);
+
+    this.markdown$.pipe(debounceTime(1600)).subscribe((markdown: string): void => {
+      this.updatePostContent(this.post.slug, markdown);
+    });
+  }
 
   onChange(): void {
+    this.updatePostContent(this.post.slug, this.post.markdown);
+  }
+
+  onKeyPress(): void {
+    this.markdown$.next(this.post.markdown);
+  }
+
+  private updatePostContent(slug: string, markdown: string): void {
     this.mutate(
       `
         mutation {
@@ -18,10 +41,9 @@ export class PostMarkdownEditorComponent extends AbstractPostEditorComponent {
         }
       `,
       {
-        slug: this.post.slug,
-        markdown: this.post.markdown,
+        slug,
+        markdown,
       }
     );
   }
-
 }
