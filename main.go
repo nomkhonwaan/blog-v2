5cb838dbf028475f7897b10442601ed431f46193
Natcha Luangaroonchai <me@nomkhonwaan.com>
2019-12-29 07:41:57 +0700

    Same

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	generate_indices "github.com/nomkhonwaan/myblog/cmd/generate-indices"
 	"github.com/nomkhonwaan/myblog/cmd/serve"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -21,7 +20,7 @@ func main() {
 		Short:   "Personal blog website written in Go with Angular 2+",
 		Version: fmt.Sprintf("%s %s\n", version, revision),
 	}
-	cmd.AddCommand(generate_indices.Command, serve.Command)
+	cmd.AddCommand(serve.Command)
 
 	if err := cmd.Execute(); err != nil {
 		logrus.Fatalf("myblog: %s", err)
